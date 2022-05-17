c0e2a8721866467b5bfaf478dfb25fc607b0192a
fabrizioCast <waynesleek3@gmail.com>
2022-05-16 20:11:43 -0500

    first commit
    :)

---
+++ src/cibertec/main.java
@@ -0,0 +1,149 @@
+package cibertec;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+
+public class main extends JFrame {
+	
+	// Variables
+	public static String modelo0 = "Aviator";
+	public static double precio0 = 124.9;
+	public static double ancho0 = 63.5;
+	public static double alto0 = 30.9;
+	public static double fondo0 = 29.2;
+	// Datos mínimos del segundo maleta
+	public static String modelo1 = "Century";
+	public static double precio1 = 174.9;
+	public static double ancho1 = 75.0;
+	public static double alto1 = 28.5;
+	public static double fondo1 = 30.0;
+	// Datos mínimos del tercer maleta
+	public static String modelo2 = "Benneton";
+	public static double precio2 = 159.9;
+	public static double ancho2 = 65.2;
+	public static double alto2 = 31.2;
+	public static double fondo2 = 29.4;
+	// Datos mínimos del cuarto maleta
+	public static String modelo3 = "Lucas";
+	public static double precio3 = 99.9;
+	public static double ancho3 = 45.0;
+	public static double alto3 = 70.0;
+	public static double fondo3 = 25.0;
+	// Datos mínimos del quinto maleta
+	public static String modelo4 = "Samsonite";
+	public static double precio4 = 84.9;
+	public static double ancho4 = 50.2;
+	public static double alto4 = 60.4;
+	public static double fondo4 = 25.0;
+	// Porcentajes de descuento
+	public static double porcentaje1 = 7.5;
+	public static double porcentaje2 = 10.0;
+	public static double porcentaje3 = 12.5;
+	public static double porcentaje4 = 15.0;
+	// Obsequios
+	public static String tipoObsequio = "Lapicero";
+	public static int obsequioCantidad1 = 2;
+	public static int obsequioCantidad2 = 3;
+	public static int obsequioCantidad3 = 4;
+	public static String obsequio1 = "USB";
+	public static String obsequio2 = "Cuaderno";
+	public static String obsequio3 = "Lapicero";
+	// FIN VARIABLES
+
+	private JPanel contentPane;
+	private JButton btnNewButton;
+	private JMenuBar menuBar;
+	private JMenu mnNewMenu;
+	private JMenu mnNewMenu_1;
+	private JMenu mnNewMenu_2;
+	private JMenu mnNewMenu_3;
+	private JMenu mnNewMenu_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					main frame = new main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public main() {
+		setTitle("Ventas");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 698, 422);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 682, 22);
+		contentPane.add(menuBar);
+		
+		mnNewMenu = new JMenu("Archivo");
+		menuBar.add(mnNewMenu);
+		
+		mnNewMenu_1 = new JMenu("Mantenimiento");
+		menuBar.add(mnNewMenu_1);
+		
+		mnNewMenu_2 = new JMenu("Ventas");
+		menuBar.add(mnNewMenu_2);
+		
+		mnNewMenu_3 = new JMenu("Configuraci\u00F3n");
+		menuBar.add(mnNewMenu_3);
+		
+		mnNewMenu_4 = new JMenu("Ayuda");
+		menuBar.add(mnNewMenu_4);
+
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+}
